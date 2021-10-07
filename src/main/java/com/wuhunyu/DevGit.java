5480f7467a5a4545e82b5e83218e53abb8b5a9f7
wuhunyu <1632766060@qq.com>
2021-10-07 15:43:32 +0800

    修改 DevGit.java by master

--- src/main/java/com/wuhunyu/DevGit.java
+++ src/main/java/com/wuhunyu/DevGit.java
@@ -5,6 +5,8 @@ public class DevGit {
     public static void main(String[] args) {
         System.out.println("我是dev分支");
         System.out.println("我是master分支");
+
+        System.out.println("我是master分支v2");
     }
 
 }
